32eb5f7d8c13f08123c7e747b551e214f0bb298b
eureka-cpu <github.eureka@gmail.com>
2023-12-23 15:44:29 -0600

    add home-manager back into flake

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { nixpkgs, home-manager, ... }:
@@ -15,6 +19,14 @@
         inherit system;
         modules = [
           ./nixos/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useUserPackages = true;
+              useGlobalPkgs = true;
+              users.eureka = ./home-manager/home.nix;
+            };
+          }
         ];
       };
     };
