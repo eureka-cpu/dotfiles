b9c7769a66886855cd0ec810a3802c28bd6acf8b
eureka-cpu <github.eureka@gmail.com>
2023-12-24 16:16:17 -0600

    add performance driver to fix nvidia glitch issue

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -64,7 +64,7 @@
     # Enable the nvidia settings menu
     nvidiaSettings = true;
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
   # Configure keymap in X11
