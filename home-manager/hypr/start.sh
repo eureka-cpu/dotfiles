c1bcb9a4aa0da1678dddff10731389508ac0c18a
eureka-cpu <github.eureka@gmail.com>
2023-12-24 15:15:13 -0600

    add eww bar to start.sh

--- home-manager/hypr/start.sh
+++ home-manager/hypr/start.sh
@@ -3,6 +3,8 @@
 # init wallpaper daemon & set wallpaper
 sleep 2; swww init & swww img ~/Wallpapers/gruvbox-mountain-village-6k.jpg &
 
+eww daemon -c ~/Code/mynixui/eww & eww open window -c ~/Code/mynixui/eww &
+
 nm-applet --indicator &
 
 # notification daemon
