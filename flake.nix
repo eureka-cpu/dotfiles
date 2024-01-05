0c1c2c53446782356e2c44969513bc94ccc11c26
eureka-cpu <github.eureka@gmail.com>
2024-01-04 22:25:59 -0600

    wip add helix-themes

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    helix-themes.url = "github:eureka-cpu/helix-themes.nix";
   };
 
   outputs = { nixpkgs, home-manager, ... }:
