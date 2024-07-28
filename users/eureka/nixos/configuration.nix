5eade1f95cf3643452b39f1c40b6959e1643927c
eureka-cpu <github.eureka@gmail.com>
2024-07-27 19:11:22 -0700

    update lock file, rm deprecated sound.enable option

--- users/eureka/nixos/configuration.nix
+++ users/eureka/nixos/configuration.nix
@@ -54,7 +54,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
