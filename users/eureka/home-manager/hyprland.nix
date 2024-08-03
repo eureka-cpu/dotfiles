dfd3e367a701cae63a0d5792251f8679242ae186
eureka-cpu <github.eureka@gmail.com>
2024-08-03 13:36:00 -0700

    fix critter-tank monitor setup

--- users/eureka/home-manager/hyprland.nix
+++ users/eureka/home-manager/hyprland.nix
@@ -47,12 +47,12 @@
         #
         # Default:
         # monitor = ",preferred,auto,auto";
-        # TODO: Fix multi-monitor setup
-        # "desc:ESP eD15T(2022), preferred, -1080x0, 1, transform, 1"
         monitor = [
           "desc:HP Inc. HP Z32 CN42411R5T, preferred, auto, 1.5"
+          "desc:ESP eD15T(2022) 0x00011916, preferred, 0x0, 1.5, transform, 1"
           "Unknown-1, disabled" # fix for upstream wl-roots bug
         ];
+        workspace = "1, monitor:ESP eD15T(2022) 0x00011916, default:true, persistent:true";
 
         ###################
         ### MY PROGRAMS ###
@@ -76,9 +76,9 @@
         # exec-once = waybar & hyprpaper & firefox
         exec-once = [
           "${onStart}/bin/start.sh"
-          (openOnWorkspace 1 "$terminal")
-          (openOnWorkspace 1 "$browser")
           (openOnWorkspace 2 "$terminal")
+          (openOnWorkspace 2 "$browser")
+          (openOnWorkspace 1 "$terminal")
           "hyprctl dispatch workspace 2"
           "hyprctl dispatch workspace 1"
         ];
