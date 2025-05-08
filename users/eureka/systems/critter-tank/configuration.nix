de75343f2cc1d6d079e575a1c4c82a5e3b571dc0
eureka-cpu <github.eureka@gmail.com>
2025-05-08 13:26:41 -0700

    Add gamemode program to critter-tank

--- users/eureka/systems/critter-tank/configuration.nix
+++ users/eureka/systems/critter-tank/configuration.nix
@@ -42,6 +42,7 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+  programs.gamemode.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
